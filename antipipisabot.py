83ba4d3340ee07bb4a209b9d6f3854164b9c1eaf
MaxMorfiX <offmax.me@gmail.com>
2026-01-09 18:58:36 +0100

    made logging more robust, consistent and fixed the bug with the wrong peer id

--- antipipisabot.py
+++ antipipisabot.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pyrogram.types import Message
 
-from config import API_ID, API_HASH, PHONE_NUMBER, PASSWORD, FORWARD_DELETED_MESSAGES, PIPISABOT_USER_ID, ENABLE_TELEGRAM_LOGS, CHAT_FOR_LOGS_ID
+from config import API_ID, API_HASH, PHONE_NUMBER, PASSWORD, FORWARD_DELETED_MESSAGES, PIPISABOT_USER_ID, TELEGRAM_LOGS
 app = Client("Antipipisabot", api_id=API_ID, api_hash=API_HASH, phone_number=PHONE_NUMBER, password=PASSWORD)
 # app = Client("Antipipisabot", api_id=API_ID, api_hash=API_HASH)
 
@@ -46,53 +46,62 @@ async def got_message_from_bot(client: 'Client', message: 'Message'):
     if not message.text:
         message.text = message.caption or "none"
     
-    log(f"\ngot message in group {message.chat.title} ({message.chat.id}) from {message.from_user.first_name} ({message.from_user.id}) with id {message.id}:")
-    log(message.text)
+    await log(f"\ngot message in group {message.chat.title} ({message.chat.id}) from {message.from_user.first_name} ({message.from_user.id}) with id {message.id}:")
+    await log(message.text)
     
     if message.text == "none": return
     
     for phrase in bot_phrases_to_pass:
         if phrase not in message.text: continue
         
-        log(f"matched the string '{phrase}', exiting the function")
+        await log(f"matched the string '{phrase}', exiting the function")
         return
 
-    log("didn't find any match, the message would be deleted")
+    await log("didn't find any match, the message would be deleted")
     
     await delete_message(message)
 
 async def delete_message(message: 'Message'):
     
-    log("Deleting...")
+    await log("Deleting...")
     
     try:
         if(FORWARD_DELETED_MESSAGES): await message.forward(chat_id=FORWARD_DELETED_MESSAGES)
-        #from https://docs.pyrogram.org/faq/peer-id-invalid-error:
-        #"Peer id invalid" error could mean, that the chat id refers to a user or chat your current session hasn’t met yet.
-        #idk what would be an elegant way to fix this
     except Exception as e:
-        app.send_message(FORWARD_DELETED_MESSAGES, f"#error occured during message forwarding: {e}")
+        await log(e)
     
     try:
         await message.delete()
-        app.send_message(FORWARD_DELETED_MESSAGES, "#deleted successfully\nСообщение было удалено успешно!")
+        await log("#deleted successfully\nСообщение было удалено успешно!", True)
     except Exception as e:
-        log(f"#error occured during message deletion: {e}")
+        await log(f"#error occured during message deletion: {e}", True)
     
-async def log(text: str):
+async def log(text: str, in_forwarded_chat_too: bool = False):
     
     print(text)
     
-    if(not ENABLE_TELEGRAM_LOGS): return
+    if in_forwarded_chat_too:
+        if FORWARD_DELETED_MESSAGES: await app.send_message(FORWARD_DELETED_MESSAGES, text)
     
-    await app.send_message(CHAT_FOR_LOGS_ID, text)
+    if(not TELEGRAM_LOGS): return
+    
+    await app.send_message(TELEGRAM_LOGS, text)
 
 async def after_startup():
      
     print("Connecting to server...")
     
     async with app:
-        log("Application is online!")
+        
+        print("Initializing chats...")
+        
+        async for dialog in app.get_dialogs():
+            pass
+        #^this is fix for the https://docs.pyrogram.org/faq/peer-id-invalid-error error
+        #where the chat id refers to a user or chat client current session hasn’t met yet,
+        #so in the beginning of each session I get all chats the user has so that this error has no chance of occuring
+
+        await log("Application is online!")
        
         await idle()
 
